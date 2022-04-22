b95633b2fef02044afe1f8da1924cd7fa834056f
Andres Marquez <andresmftech5@gmail.com>
2022-04-21 21:49:13 -0500

    Creating the main table of the project

---
+++ src/components/tableBody.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from "react";
+import Coins from "./coins";
+
+const TableBody = () => {
+
+    const [coins, setCoins] = useState([])
+
+    const getData = async () => {
+        const data = await fetch('https://api.binance.com/api/v3/ticker/price')
+        const user = await data.json()
+        setCoins(user)
+    }
+
+    useEffect(() => {
+        
+        setInterval(() => {
+            getData()
+        }, 5000);
+        
+    },[])
+
+    return(
+        <tbody>
+            {
+                coins.sort(function(a,b){
+                    let a2 = a.price;
+                    let b2 = b.price;
+                    return b2 - a2;
+                }).map(element =>
+                    <Coins key={element.symbol} coins={element} />
+                )
+            }
+        </tbody>
+    );
+}
+
+export default TableBody;

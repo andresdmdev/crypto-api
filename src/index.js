beda3c41cca1308c624c0bd4b23f64e3e5a428e2
Andres Marquez <andresdmf55@gmail.com>
2022-08-09 11:16:34 -0500

    Change main files, adding new depencies and styles

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './styles/index.css';
 import App from './App';
 import { Provider } from 'react-redux';
-import { store }from  './api/store';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { store }from  './services/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(

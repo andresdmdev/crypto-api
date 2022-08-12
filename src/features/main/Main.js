9e03e58c42e5ddf1d466e79a14c457b26215416d
Andres Marquez <andresdmf55@gmail.com>
2022-08-12 13:50:09 -0500

    Change some components with their styles

--- src/features/main/Main.js
+++ src/features/main/Main.js
@@ -1,18 +1,15 @@
 import React from "react";
-import Presentation from "./Presentation";
 import Table from "./table/Table";
 import '../../styles/App.css'
 import Form from "../header/Form";
-import PresentationMovil from "./PresentationMovil";
 import SliderNews from "./slider/SliderNews";
 
 export default function Main(){
 
   return(
     <main className="main">
-      {/* <Presentation /> */}
+      <h1 className="main_title">Today's Cryptocurrency Prices</h1>
       <SliderNews />
-      <PresentationMovil />
       <Form web={false} />
       <Table />
     </main>
--- src/features/main/PresentationMovil.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import '../styles/stylesPresentation.css'
-import imageMovil from '../../img/image-movil.png'
-
-export default function PresentationMovil(){
-  return (
-    <img src={imageMovil} alt="coins-movil" className="main_presentation_movil" />
-  )
-}
